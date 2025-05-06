75c10799723943de7a0f8190d3f61c3a4ed01abf
Anis25D <anisdms25@gmail.com>
2025-05-06 01:16:14 +0100

    Initial commit

---
+++ Server/mon_projet/urls.py
@@ -0,0 +1,26 @@
+"""mon_projet URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from elearning import views
+from django.contrib import admin
+from django.urls import path,include
+
+urlpatterns = [
+    path('predecte/', views.predict_view),
+    path('registre/',views.registration.as_view()),
+    path('login/',views.login.as_view()),
+    path("historique/", views.historique.as_view()),
+    path("historique/<int:author_id_id>/", views.gethistorique, name="delete-note"),
+]
